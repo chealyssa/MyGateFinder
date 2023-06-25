e97c7bc69136b9dd121dc113e52b8ac6037375ed
Che Alyssa Zulaik <81701327+chealyssa@users.noreply.github.com>
2023-06-25 02:08:07 -0500

    removed hello world

--- Main.java
+++ Main.java
@@ -12,7 +12,6 @@ import java.util.*;
 class Main {
   public static void main(String[] args) {
     Scanner kb = new Scanner(System.in);
-    System.out.println("Hello world!");
     startNote(kb); 
   }
 
